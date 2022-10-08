3c50d62c9289a38c95c384b6fab5c2899e8e3d2d
mironiukzb <108904629+mironiukzb@users.noreply.github.com>
2022-10-08 11:50:03 +0200

    add csrf protection

--- src/Controller/Api/AbstractApiController.php
+++ src/Controller/Api/AbstractApiController.php
@@ -9,6 +9,10 @@ abstract class AbstractApiController extends AbstractController
 {
     protected function buildForm(string $type, $data = null, array $options = []) : FormInterface
     {
+//         $options = array_merge($options, [
+//            'csrf_protection' => false,
+//         ]);
+        
         return $this->container->get('form.factory')->createNamed('', $type, $data, $options);
     }
 }
